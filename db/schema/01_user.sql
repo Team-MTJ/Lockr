82694089385f87464eaf76a4a791f6fccf65f3bc
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-03 19:33:37 -0400

    make schemas from ERD

---
+++ db/schema/01_user.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS user
+CASCADE;
+
+CREATE TABLE user
+(
+  id       SERIAL       PRIMARY KEY,
+  name     VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email    VARCHAR(255) NOT NULL
+);
---
+++ db/schema/04_pwd.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS pwd
+CASCADE;
+
+CREATE TABLE pwd
+(
+  id            SERIAL       PRIMARY KEY,
+  org_id        INTEGER      REFERENCES org ON DELETE CASCADE,
+  website_title VARCHAR(255) NOT NULL,
+  website_url   VARCHAR(255) NOT NULL,
+  encrypted_pwd TEXT         NOT NULL,
+  category      VARCHAR(255)
+);
