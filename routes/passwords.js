77837e680daa60fa9929301a11c9a81533e8b0dc
xynyx <matt.taylr22@gmail.com>
2020-04-04 23:37:27 +0000

    Complete route for modal authorization

--- routes/passwords.js
+++ routes/passwords.js
@@ -5,15 +5,14 @@ module.exports = (db) => {
   // Use for passwords already created, and for new passwords
   const dbHelpers = require("./helpers/db_helpers")(db);
 
-  router.get("/password/:pwd", (req, res) => {
+  router.get("/:pwd_id", (req, res) => {
     // get user_id from req.session
     const { userId } = req.session;
-    const password = req.params.pwd;
-    if (dbHelpers.isAuthorized(userId, password)) {
-      
-    } else {
-      return res.send("You do not have access to this password.");
-    }
+    const password = req.params.pwd_id;
+    dbHelpers.isAuthorized(userId, password) .then((data) => {
+      res.json( {data});
+    })
+    .catch((e) => console.error(e));
   });
 
   return router;
