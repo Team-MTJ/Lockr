f9390d9a55c566e6177f99d69361d06587fb1ef7
xynyx <matt.taylr22@gmail.com>
2020-04-05 20:30:14 +0000

    Change modal id to class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,12 +12,12 @@ $(() => {
 });
 
 // Copy to clipboard function for modal
-$("#copy").on("click", function() {
+$(".copy").on("click", function() {
   // Remove disable to allow copy function
-  $('#passwordBox').prop("disabled", false);
-  const $text = $("#passwordBox");
+  $('.passwordBox').prop("disabled", false);
+  const $text = $(".passwordBox");
   $text.select();
   document.execCommand("copy");
   // Enable disable again
-  $('#passwordBox').prop("disabled", true);
+  $('.passwordBox').prop("disabled", true);
 });
