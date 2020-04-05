abe3189d2b6529b06ade89938e2d070f3801e2a2
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-04-05 13:08:50 -0400

    works as intended

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,5 +1,26 @@
 const generateRandomPassword = (options) => {
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  const numbers = '1234567890';
+  const numbers = "1234567890";
+  const symbols = "~!@#$%^&*()_+-=`;?/.,<>[]{}";
 
+  let charPool = ""; // The possible characters to be included in password
+  charPool += letters;
+
+  if (options.includeNumbers) {
+    charPool += numbers;
+  }
+
+  if (options.includeSymbols) {
+    charPool += symbols;
+  }
+
+  let password = "";
+
+  for (let i = 0; i < options.length; i++) {
+    const randomCharIndex = Math.floor(Math.random() * charPool.length);
+    const randomChar = charPool.charAt(randomCharIndex);
+    password += randomChar;
+  }
+
+  return password;
 };
