d8f6c4977ac89814d26873ad8af8a440f854b4c9
xynyx <matt.taylr22@gmail.com>
2020-04-05 18:42:43 +0000

    Add initial copy to clipboard function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,43 +8,11 @@ $(() => {
     }
   });
 
-  const $password = $("#open-pwd");
-  $password.on("click", function () {
-    // e.preventDefault();
-    const title = $(this).data("title");
-    // const a = $(this);
-    // const modal = $("#modal");
-    $("#title").val(title)
-    $('#modal').modal('show');
+  $("#copy").on("click", function() {
+    console.log("WTF")
+    const $text = $("#passwordBox");
+    $text.focus().select();
+    document.execCommand("copy");
+    alert("Copied the text: " + $text.value);
   });
-
-  $('#modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
-  })
-
-
-
 });
-
-/*     $.getJSON('/passwords') + a.data("id"), function(data) {
-      $("#title").val($(this).data('title'))
-    } */
-
-/*     $.ajax({
-      url: "/passwords/:pwd_id",
-      method: "GET",
-      dataType: "json",
-      success: (pwd) => {
-        $("#pwd-info").html(pwd);
-        $("#modal").modal("show");
-      },
-    }); */
-
-
-  
