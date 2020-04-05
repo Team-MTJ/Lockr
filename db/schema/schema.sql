fef4f56efc575c0de7ebc3c08af59de12f0ad8e5
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-04 20:44:20 -0400

    better do npm run db:reset to import all sql files

--- db/schema/schema.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-\i ./db/schema/01_users.sql
-\i ./db/schema/02_org.sql
-\i ./db/schema/03_membership.sql
-\i ./db/schema/04_pwd.sql
-\i ./db/schema/05_usage.sql
-COMMIT;
--- psql -d midterm -U labber -f ./db/schema/schema.sql
