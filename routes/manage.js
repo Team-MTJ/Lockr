6a0c5f10a1afd285d3c9b703c935117320bc93d9
xynyx <matt.taylr22@gmail.com>
2020-04-06 12:30:33 -0400

    Acquire orgs where user is admin only

--- routes/manage.js
+++ routes/manage.js
@@ -10,18 +10,32 @@ module.exports = (db) => {
     dbHelpers.getUserWithId(req.session.userId).then((user) => {
       dbHelpers.getOrgsWithUserId(user.id).then((orgs) => {
         // If user has no orgs they're a part of of, redirect to "Create new org"
-        if (!orgs) return res.redirect("/orgs/new");
+        if (!orgs) res.redirect("/orgs/new");
         // if memberOfAnyOrgs -> check each for admin privileges and add to array
         // Only want to display orgs where they have admin rights on the manage page
         let orgsWhereUserIsAdmin = [];
+        // console.log(orgs);
         orgs.forEach((row) => {
-          if (dbHelpers.isUserAdmin(row.org_id, row.user_id)) {
+          // console.log("***", row, "***")
+          // console.log("ORG ID", row.org_id)
+          // console.log("USER ID", row.user_id)
+          if (row.is_admin) {
             orgsWhereUserIsAdmin.push(row);
           }
+          // dbHelpers.isUserAdmin(row.org_id, row.user_id).then(adminOrNot => {
+          //   orgsWhereUserIsAdmin.push(adminOrNot);
+          // })
         });
+        console.log(orgsWhereUserIsAdmin);
+        // console.log(orgsWhereUserIsAdmin);
         if (orgsWhereUserIsAdmin.length === 0) {
-          return res.status(400).send("You have no admin privileges in any organization that you belong to.");
+          return res
+            .status(400)
+            .send(
+              "You have no admin privileges in any organization that you belong to."
+            );
         } else {
+          dbHelpers.getUsersByOrg
           const templateVars = { user, orgsWhereUserIsAdmin, orgs };
           res.render("manage", templateVars);
         }
