a0ac62dd5a2d7baab379dcc99e836d62a49d5019
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-08 13:51:00 -0400

    can modify timespwned attribute of passwordBox

--- public/scripts/organization.js
+++ public/scripts/organization.js
@@ -1,10 +1,11 @@
 // Client-side javascript loaded into only organization.ejs
 /**
- * Returns number of times password has been pwned using haveibeenpwned API
+ * Adds attribute timespwned=number to each .passWordBox <input> using haveibeenpwned API upon opening a modal
  * @param {String} password The password to check
  * @return {Number} How many times pwned according to API
  */
-const timesPwned = function (password) {
+const addPwnedAttributeToPasswordBox = function ($passwordBox) {
+  const password = $passwordBox.val();
   const hashedPassword = sha1(password).toUpperCase();
   const firstFiveHash = hashedPassword.slice(0, 5);
   const remainingHash = hashedPassword.slice(5, hashedPassword.length);
@@ -15,16 +16,34 @@ const timesPwned = function (password) {
       // data is one string with remainingHash:noOfTimes on each line
       const rows = data.split(/\n|:/);
       const indexOfPassword = rows.indexOf(remainingHash);
+
+      // Change timespwned attribute on the input class
       if (indexOfPassword === -1) {
-        console.log(0);
+        $passwordBox.attr("timespwned", 0);
       } else {
-        console.log(rows[indexOfPassword + 1]);
+        $passwordBox.attr("timespwned", rows[indexOfPassword + 1]);
       }
     },
-    error: () => {
-      return 0;
+    error: (e) => {
+      console.log(e);
     },
   });
 };
 
-timesPwned("qwerty");
+$(() => {
+  // Add timespwned='number' to all .passwordBox <inputs> upon page load
+  $(".passwordBox").each(function () {
+    addPwnedAttributeToPasswordBox($(this));
+  });
+
+  // Test code for changing of attribute when change password button is clicked
+  $(".change").on("click", function () {
+    const $passwordBox = $(event.target).parents().siblings(".passwordBox");
+    addPwnedAttributeToPasswordBox($passwordBox);
+  });
+
+  // Changes timespwned as passwordBox is being modified
+  $(".passwordBox").on("keyup", function () {
+    addPwnedAttributeToPasswordBox($(this));
+  });
+});
