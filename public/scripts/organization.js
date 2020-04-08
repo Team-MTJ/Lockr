f2797c5a680ef921b670694b1ee0f5b431a9d1ec
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-08 12:46:34 -0400

    prints number of times password was breached

--- public/scripts/organization.js
+++ public/scripts/organization.js
@@ -1,2 +1,30 @@
 // Client-side javascript loaded into only organization.ejs
+/**
+ * Returns number of times password has been pwned using haveibeenpwned API
+ * @param {String} password The password to check
+ * @return {Number} How many times pwned according to API
+ */
+const timesPwned = function (password) {
+  const hashedPassword = sha1(password).toUpperCase();
+  const firstFiveHash = hashedPassword.slice(0, 5);
+  const remainingHash = hashedPassword.slice(5, hashedPassword.length);
+  $.ajax({
+    url: `https://api.pwnedpasswords.com/range/${firstFiveHash}`,
+    method: "GET",
+    success: (data) => {
+      // data is one string with remainingHash:noOfTimes on each line
+      const rows = data.split(/\n|:/);
+      const indexOfPassword = rows.indexOf(remainingHash);
+      if (indexOfPassword === -1) {
+        console.log(0);
+      } else {
+        console.log(rows[indexOfPassword + 1]);
+      }
+    },
+    error: () => {
+      return 0;
+    },
+  });
+};
 
+timesPwned("qwerty");
