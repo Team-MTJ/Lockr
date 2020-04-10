8f2239d69f32e7b89a18c27e082228e44bfdb0e2
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-10 10:13:44 -0400

    change animal names to group member names

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 INSERT INTO users
   (first_name, last_name, email, password)
 VALUES
-  ('Aaron', 'Aardvark', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Bob', 'Barker', 'b@b.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-  ('Curious', 'Cat', 'c@c.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Aaron', 'Aaronson', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Jimmy', 'Peng', 'j@j.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Matt', 'Taylor', 'm@m.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
   ('Danger', 'Doge', 'd@d.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
