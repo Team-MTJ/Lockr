b4e0437dc51095aa7c68d2ce7447f478176cb8e8
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-10 14:59:02 +0000

    fixed alert

--- public/scripts/emailVerification.js
+++ public/scripts/emailVerification.js
@@ -6,7 +6,13 @@ $(() => {
       `http://apilayer.net/api/check?access_key=05c4fc16024b99d205328b12f3945f68&email=${registerEmail}&smtp=0`,
       (data) => {
         if (!data.mx_found) {
-          console.log("Not found!");
+          $(".email_alert").html(`
+          <div class="alert alert-warning alert-dismissible fade show invalidEmail" role="alert">
+          <strong>Invalid</strong> Email!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>`);
         } else {
           $("#register-form").submit();
         }
